35fa97fcc6aa84749b9f453005c76a8ee19b1603
John Liu <elite_jigang@163.com>
2012-11-25 18:20:40 +0800

    Add a few files for test

--- src/main/java/com/ilucene/SearchService.java
+++ src/main/java/com/ilucene/SearchService.java
@@ -2,23 +2,27 @@ package com.ilucene;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Vector;
 
 import com.ilucene.util.CommonUtil;
 import com.ilucene.util.FileUtils;
+import junit.framework.Assert;
 import org.apache.lucene.analysis.SimpleAnalyzer;
 import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,6 +39,7 @@ public class SearchService {
                                        "Great city"};
     protected String[] text = { "Venice","Amsterdam", "Venice"};
     private Directory directory;
+    private IndexSearcher searcher;
     /**
      * Initializes the index data directory path
      */
@@ -87,8 +92,8 @@ public class SearchService {
         IndexSearcher searcher = new IndexSearcher(directory);
         Term t = new Term(fieldName, searchString);
         Query query = new TermQuery(t);
-        int hitCount = CommonUtil.hitCount(searcher, query);
-        logger.debug("hitCount: "+ hitCount);
+        int hitCount = CommonUtil.hitCount(searcher, query);  // equels "search(query, 1).totalHits"
+        logger.debug("search " + query + " hitCount: "+ hitCount);
         searcher.close();
         return hitCount;
     }
@@ -102,7 +107,7 @@ public class SearchService {
                 IndexWriter.MaxFieldLength.UNLIMITED);
     }
     /**
-     * Searches the content with specified filed.
+     * Searches the content with specified TermQuery .
      * @param filed
      *         document filed in lucene
      * @param content
@@ -122,6 +127,35 @@ public class SearchService {
         }
     }
 
+    /**
+     * Searches the content with specified QueryParser .
+     * @param filed
+     *         document filed in lucene
+     * @param content
+     *         content value with String type
+     */
+    public TopDocs queryParser(String filed, String content){
+        TopDocs docs = null;
+        try {
+            Directory dir = FSDirectory.open(new File(indexDir));
+            searcher = new IndexSearcher(dir);
+            QueryParser parser = new QueryParser(Version.LUCENE_30,      //A
+                    filed,                  //A
+                    new SimpleAnalyzer());       //A
+
+            try {
+                Query query = parser.parse(content);                  //B
+                docs = searcher.search(query, 10);
+                logger.info(content + "masScore" + docs.getMaxScore());
+                logger.info(filed + "Total hits: " + docs.totalHits);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            logger.error("Failed to open directory ", e );
+        }
+        return docs;
+    }
     /**
      *  A module indexes the author array
      * @throws Exception
@@ -161,6 +195,7 @@ public class SearchService {
         IndexWriter writer1 = new IndexWriter(dir, new SimpleAnalyzer(),
                                        IndexWriter.MaxFieldLength.UNLIMITED);
         IndexWriter writer2 = null;
+        /*--The IndexWriter 2 stance will never be done because there is an IndexWriter that locks the operation.*/
         try {
             writer2 = new IndexWriter(dir, new SimpleAnalyzer(),
                     IndexWriter.MaxFieldLength.UNLIMITED);
@@ -187,16 +222,242 @@ public class SearchService {
                 IndexWriter.MaxFieldLength.UNLIMITED);
 
         writer.setInfoStream(System.out);
-
+        logger.debug("Optimise the verbose index filed");
         for (int i = 0; i < 100; i++) {
             Document doc = new Document();
             doc.add(new Field("keyword", "goober", Field.Store.YES, Field.Index.NOT_ANALYZED));
+            writer.addDocument(doc);
+        }
+        /**--It does many things on the hood.---**/
+        writer.optimize();
+        writer.close();
+    }
+
+
+    protected void validateQueryParser(){
+        TopDocs topDocs = queryParser("contents", "+ArgoUML +UML -Eclipse");
+        Assert.assertTrue(topDocs.totalHits == 1)  ;
+        try {
+            Document doc = searcher.doc(topDocs.scoreDocs[0].doc);
+            Assert.assertEquals("contents:+ArgoUML +UML -Eclipse", "ArgoUML-1.txt", doc.get("fileName"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        TopDocs topDocs1 = queryParser("contents", "ArgoUML AND UML OR Eclipse");
+        Assert.assertTrue(topDocs1.totalHits == 2)  ;
+    }
+
+    /**
+     * Searches the content near real time.
+     * /*
+     #1 Create near-real-time reader
+     #A Wrap reader in IndexSearcher
+     #B Search returns 10 hits
+     #2 Delete 1 document
+     #3 Add 1 document
+     #4 Reopen reader
+     #5 Confirm reader is new
+     #6 Close old reader
+     #7 Verify 9 hits now
+     #8 Confirm new document matched
+     */
+    protected void searchNRT() throws IOException {
+        Directory dir = new RAMDirectory();
+        IndexWriter writer = new IndexWriter(dir,
+                                   new StandardAnalyzer(Version.LUCENE_30),
+                                   IndexWriter.MaxFieldLength.UNLIMITED);
+        for(int i=0;i<10;i++) {
+            Document doc = new Document();
+            doc.add(new Field("id", ""+i,
+                    Field.Store.NO,
+                    Field.Index.NOT_ANALYZED_NO_NORMS));
+            doc.add(new Field("text", "aaa",
+                    Field.Store.NO,
+                    Field.Index.ANALYZED));
+            writer.addDocument(doc);
+        }
+        IndexReader reader = writer.getReader();                 // #1
+        IndexSearcher searcher = new IndexSearcher(reader);      // #A
+
+        Query query = new TermQuery(new Term("text", "aaa"));
+        TopDocs docs = searcher.search(query, 1);
+        Assert.assertEquals(10, docs.totalHits);                        // #B
+
+        writer.deleteDocuments(new Term("id", "7"));             // #2
+
+        Document doc = new Document();                           // #3
+        doc.add(new Field("id",                                  // #3
+                "11",                                  // #3
+                Field.Store.NO,                        // #3
+                Field.Index.NOT_ANALYZED_NO_NORMS));   // #3
+        doc.add(new Field("text",                                // #3
+                "bbb",                                 // #3
+                Field.Store.NO,                        // #3
+                Field.Index.ANALYZED));                // #3
+        writer.addDocument(doc);                                 // #3
+
+        IndexReader newReader = reader.reopen();                 // #4
+        Assert.assertFalse(reader == newReader);                        // #5
+        reader.close();                                          // #6
+        searcher = new IndexSearcher(newReader);
+
+        TopDocs hits = searcher.search(query, 10);               // #7
+        Assert.assertEquals(9, hits.totalHits);                         // #7
+
+        query = new TermQuery(new Term("text", "bbb"));          // #8
+        hits = searcher.search(query, 1);                        // #8
+        Assert.assertEquals(1, hits.totalHits);                         // #8
+
+        newReader.close();
+        writer.close();
+    }
+
+    protected void queryByPhrase() throws IOException {
+        Directory dir;
+        IndexSearcher searcher;
+        dir = new RAMDirectory();
+        IndexWriter writer = new IndexWriter(dir,
+                new WhitespaceAnalyzer(),
+                IndexWriter.MaxFieldLength.UNLIMITED);
+        Document doc = new Document();
+        doc.add(new Field("field",                                    // 1
+                    "the quick brown fox jumped over the lazy dog",     // 1
+                    Field.Store.YES,                                    // 1
+                    Field.Index.ANALYZED));                             // 1
+        writer.addDocument(doc);
+        writer.close();
+
+        searcher = new IndexSearcher(dir);
+
+        PhraseQuery query = new PhraseQuery();              // 2
+        String[] phrase = new String[] {"quick", "fox"};
+        for (String word : phrase) {             // 3
+            query.add(new Term("field", word));          // 3
+        }                                                   // 3
+        query.setSlop(1);                                // 2
+        TopDocs matches = searcher.search(query, 10);
+        Assert.assertTrue( matches.totalHits == 1);
+    }
+
+    protected void indexSingleFieldDocs(Field[] fields) throws IOException {
+        directory = new RAMDirectory();
+        IndexWriter writer = new IndexWriter(directory, new WhitespaceAnalyzer(),
+                                             IndexWriter.MaxFieldLength.UNLIMITED);
+        for (Field f : fields) {
+            Document doc = new Document();
+            doc.add(f);
             writer.addDocument(doc);
         }
         writer.optimize();
         writer.close();
     }
 
+    protected void searchByWildcard() throws IOException {
+        directory  = new RAMDirectory();
+        indexSingleFieldDocs(new Field[]
+                { new Field("contents", "wild", Field.Store.YES, Field.Index.ANALYZED),
+                        new Field("contents", "child", Field.Store.YES, Field.Index.ANALYZED),
+                        new Field("contents", "mild", Field.Store.YES, Field.Index.ANALYZED),
+                        new Field("contents", "mildew", Field.Store.YES, Field.Index.ANALYZED) });
+
+        IndexSearcher searcher = new IndexSearcher(directory);
+        Query query = new WildcardQuery(new Term("contents", "?ild*"));  //#A
+        TopDocs matches = searcher.search(query, 10);
+        logger.debug( "--------------------------" + matches.totalHits);
+        Assert.assertEquals("child no match", 3, matches.totalHits);
+
+        Assert.assertEquals("score the same", matches.scoreDocs[0].score,
+                matches.scoreDocs[1].score, 0.0);
+        Assert.assertEquals("score the same", matches.scoreDocs[1].score,
+                matches.scoreDocs[2].score, 0.0);
+        searcher.close();
+        directory.close();
+    }
+
+    protected void searchByFuzzy() throws IOException {
+        directory  = new RAMDirectory();
+        indexSingleFieldDocs(new Field[] { new Field("contents",
+                        "fuzzy",
+                        Field.Store.YES,
+                        Field.Index.ANALYZED),
+                        new Field("contents",
+                        "wuzzy",
+                        Field.Store.YES,
+                        Field.Index.ANALYZED)
+        });
+
+        IndexSearcher searcher = new IndexSearcher(directory);
+        Query query = new FuzzyQuery(new Term("contents", "wuzza"));
+        TopDocs matches = searcher.search(query, 10);
+        Assert.assertEquals("both close enough", 2, matches.totalHits);
+        logger.debug("matches.scoreDocs[0].score: " + matches.scoreDocs[0].score+ "\n" +
+                      "matches.scoreDocs[1].score:" + matches.scoreDocs[1].score);
+        Assert.assertTrue("wuzzy closer than fuzzy",
+                matches.scoreDocs[0].score != matches.scoreDocs[1].score);
+
+        Document doc = searcher.doc(matches.scoreDocs[0].doc);
+        Assert.assertEquals("wuzza bear", "wuzzy", doc.get("contents"));
+        searcher.close();
+    }
+
+    public static class SimpleSimilarity extends Similarity {
+        public float lengthNorm(String field, int numTerms) {
+            return 1.0f;
+        }
+
+        public float queryNorm(float sumOfSquaredWeights) {
+            return 1.0f;
+        }
+
+        public float tf(float freq) {
+            return freq;
+        }
+
+        public float sloppyFreq(int distance) {
+            return 2.0f;
+        }
+
+        public float idf(Vector terms, Searcher searcher) {
+            return 1.0f;
+        }
+
+        public float idf(int docFreq, int numDocs) {
+            return 1.0f;
+        }
+
+        public float coord(int overlap, int maxOverlap) {
+            return 1.0f;
+        }
+    }
+
+    /**
+     * <p>This is intended to be used in developing Similarity implementations,
+     * and, for good performance, should not be displayed with every hit.
+     * Computing an explanation is as expensive as executing the query over the
+     * entire index.
+     * </p>
+     * @throws IOException
+     */
+    protected void doSimpleSearch() throws IOException {
+        directory  = new RAMDirectory();
+        indexSingleFieldDocs(new Field[] {new Field("contents", "x z", Field.Store.YES, Field.Index.ANALYZED),
+                                           new Field("contents", "w x y", Field.Store.YES, Field.Index.ANALYZED)});
+        IndexSearcher searcher = new IndexSearcher(directory);
+        searcher.setSimilarity(new SimpleSimilarity());
+
+        Query query = new TermQuery(new Term("contents", "x"));
+        Explanation explanation = searcher.explain(query, 0);
+        logger.debug("[TermQuery(new Term(\"contents\", \"x\"));]:" + explanation);
+
+        TopDocs matches = searcher.search(query, 10);
+        logger.debug("doc count: " +  matches.totalHits);
+        //Assert.assertEquals(1, matches.totalHits);
+        logger.debug("matches.scoreDocs[0].score: " + matches.scoreDocs[0].score);
+       // Assert.assertEquals(1F, scroe_0, 0.0);
+        logger.debug("matches.scoreDocs[0].score: " +  matches.scoreDocs[1].score);
+
+        searcher.close();
+    }
 	/**
 	 * @param args
 	 * @throws Exception
@@ -212,9 +473,23 @@ public class SearchService {
                         + System.getProperty("java.io.tmpdir", "tmp")
                        + "\nindex directory:" +System.getProperty("user.home") );
         }
+        //Validates the QueryParser
+        service.validateQueryParser();
+
+        // search near real time
+        service.searchNRT();
+
+        //search by phrase
+        service.queryByPhrase();
+
+        //search by fuzzy
+        service.searchByFuzzy();
+
+        //Does s simple search
+        service.doSimpleSearch();
         /* Display the lock mechanism for IndexWriter*/
-        service.lockMechanism();
+      //  service.lockMechanism();
         /*-----Display verbose index----*/
-        service.showVerboseIndex();
+      //  service.showVerboseIndex();
 	}
 }
